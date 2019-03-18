d06c21ec29b1bf03b8254c31a263f20c298a18cf
karelmolina <edmolina4@gmail.com>
2019-03-18 16:17:30 -0600

    still have a rspec error
     Ambiguous match, found 8 elements matching visible button 'delete'

--- spec/feature/delete_bookmarks_spec.rb
+++ spec/feature/delete_bookmarks_spec.rb
@@ -6,9 +6,9 @@ RSpec.feature 'Deleting a Bookmark' do
     Bookmark.generate(title: 'justdelete', url: 'http://justdelete.com')
 
     visit('/bookmarks')
-
     expect(page).to have_link('justdelete',href: 'http://justdelete.com')
-    click_button 'delete'
+    click_button "delete"
+    page.has_xpath?('//bookmarks')
     expect(page).not_to have_link('justdelete', href: 'http://justdelete.com')
   end
 end
