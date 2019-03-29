6cac0af0b643cae866ae65efe2f6766bb061adf2
PalaciosZ <criszelaya24@hotmail.com>
2019-03-28 19:36:49 -0600

    Fixing logic and features test of update

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -24,7 +24,8 @@ class Bookmark
   end
   end
 
-  def self.generate(title:,url:)
+  def self.generate(title:, url:)
+    
     if ENV['ENVIROMENT'] == 'test'
       ConnectionDatabase.connect('bookmarks_manager_test')
       #@conection = PG.connect :dbname => 'bookmarks_manager_test'
