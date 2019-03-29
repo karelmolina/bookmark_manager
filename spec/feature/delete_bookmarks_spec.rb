511bbb1336b3659bd6bb331d64f73cbc649223a2
PalaciosZ <criszelaya24@hotmail.com>
2019-03-28 20:53:51 -0600

    Detectando el bookmark de otra manera y eliminandolo! BINGO!

--- spec/feature/delete_bookmarks_spec.rb
+++ spec/feature/delete_bookmarks_spec.rb
@@ -3,14 +3,12 @@ require 'spec_helper'
 RSpec.feature 'Deleting a Bookmark' do
   scenario 'delete the bookmark' do
 
-    bookmark = Bookmark.generate(title: 'justdelete', url: 'http://justdelete.com')
+    bookmark = Bookmark.generate(title: 'Academy', url: 'http://www.academy.com')
 
     visit('/bookmarks')
-    expect(page).to have_link('justdelete',href: 'http://justdelete.com')
-    first('.delete').click_button('delete')
+    expect(page).to have_link('Academy',href: 'http://www.academy.com')
+    click_button("delete #{bookmark.id}")
     expect(current_path).to eq '/bookmarks'
-    #usage: page.has_xpath('//a',:href => 'google.com')
-    #page.has_xpath?('//bookmarks/') <- NOT
-    expect(page).not_to have_content('justdelete')
+    expect(page).not_to have_link('Academy', href: "http://www.academy.com")
   end
 end
